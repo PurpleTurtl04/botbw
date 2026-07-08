49abc53e2e83fa915fb26d6351e13349b4035555
Nick Edwards <nledwa01@gmail.com>
2026-07-08 16:05:25 -0500

    Updated week select component to remove hard coded select options and automatically generate. Added state to track week on change events.

--- botbw/components/week-select.jsx
+++ botbw/components/week-select.jsx
@@ -1,21 +1,25 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function WeekSelect() {
+    const [selectedWeek, setSelectedWeek] = useState('1');
+
+    // Generate an array from 1 to 14
+    const weeks = Array.from({ length: 14 }, (_, i) => i + 1);
+
     return (
         <div>
-            <select className='select font-pressStart lg:select-lg select-primary text-primary min-h-13 min-w-68 border-3'>
-                <option>Week 1</option>
-                <option>Week 2</option>
-                <option>Week 3</option>
-                <option>Week 4</option>
-                <option>Week 5</option>
-                <option>Week 6</option>
-                <option>Week 7</option>
-                <option>Week 8</option>
-                <option>Week 9</option>
-                <option>Week 10</option>
-                <option>Week 11</option>
-                <option>Week 12</option>
-                <option>Week 13</option>
-                <option>Week 14</option>
+            <select
+                className='select font-pressStart lg:select-lg select-primary text-primary min-h-13 min-w-68 border-3'
+                value={selectedWeek}
+                onChange={(e) => setSelectedWeek(e.target.value)}
+            >
+                {weeks.map((week) => (
+                    <option key={week} value={week}>
+                        Week {week}
+                    </option>
+                ))}
             </select>
         </div>
     );
