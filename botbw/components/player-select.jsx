def6e5e4fc8d2b17728c63a99c06d8e01241e2e7
Nick Edwards <nledwa01@gmail.com>
2026-07-09 16:46:03 -0500

    Added players json and updated player select field component to map through data. Updated games json file.

--- botbw/components/player-select.jsx
+++ botbw/components/player-select.jsx
@@ -1,16 +1,23 @@
+'use client';
+
+import { useState } from 'react';
+import players from '@/data/players.json';
+
 export default function PlayerSelect() {
+    const [selectedPlayer, setSelectedPlayer] = useState('1');
+
     return (
         <div>
-            <select className='select font-pressStart lg:select-lg select-primary text-primary min-h-13 min-w-68 border-3 border-dashed'>
-                <option>Andrew</option>
-                <option>Dryden</option>
-                <option>Kurtis</option>
-                <option>Jason</option>
-                <option>Mark</option>
-                <option>Mike</option>
-                <option>Nick</option>
-                <option>Patrick</option>
-                <option>Tyler</option>
+            <select
+                className='select font-pressStart lg:select-lg select-primary text-primary min-h-13 min-w-68 border-3 border-dashed'
+                value={selectedPlayer}
+                onChange={(e) => setSelectedPlayer(e.target.value)}
+            >
+                {players.map((player) => (
+                    <option key={player.id} value={player.id}>
+                        {player.name}
+                    </option>
+                ))}
             </select>
         </div>
     );
