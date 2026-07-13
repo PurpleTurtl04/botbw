1f046560104f55723ce34f0826f8bc359f323d56
Nick Edwards <nledwa01@gmail.com>
2026-07-13 13:32:08 -0500

    Updated font size for hero text on smaller screen sizes

--- botbw/components/hero.jsx
+++ botbw/components/hero.jsx
@@ -13,9 +13,9 @@ export default function Hero() {
                 sizes='100vw'
                 className='3xl:mx-auto max-w-480 object-cover'
             />
-            <div className='hero-content mt-10 items-center'>
-                <h1 className='max-w-sm text-center text-4xl md:max-w-none md:text-[54px] lg:text-7xl 2xl:text-8xl'>
-                    <span className='font-pressStart text-base-content text-shadow-base-200 leading-relaxed text-shadow-[5px_6px_0_#000]'>
+            <div className='hero-content px-0 md:mt-10'>
+                <h1 className='xs:text-4xl text-center text-3xl sm:text-5xl md:max-w-none md:text-[54px] lg:text-7xl 2xl:text-8xl'>
+                    <span className='font-pressStart text-base-content text-shadow-base-200 leading-12 text-shadow-[5px_6px_0_#000] md:leading-20'>
                         Battle of the Benchwarmers
                     </span>
                 </h1>
